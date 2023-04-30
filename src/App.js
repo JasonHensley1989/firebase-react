bbb0dc4cf515257ad2e444dc83c491ea9874bc22
Jason Hensley <jasonhensley1989@gmail.com>
2023-04-30 12:07:24 -0500

    firestore and db creation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import { useEffect, useState } from "react";
+import Auth from "./components/auth";
+import { db } from "./firebase-config/firebase";
+import { getDocs, collection } from "firebase/firestore";
+
 function App() {
+
+  const [movieList, setMovieList] = useState([]);
+
+  const moviesCollectionRef = collection(db, "movies");
+
+  useEffect(() => {
+    const getMovieList = async() => {
+      try {
+      const data = await getDocs(moviesCollectionRef);
+      console.log(data);
+      } catch(err) {
+        console.error(err);
+      }
+    };
+    getMovieList();
+  }, [])
+
+
   return (
     <div className="App">
       <h1 className="title">Firebase React</h1>
+      <Auth />
     </div>
   );
 }
