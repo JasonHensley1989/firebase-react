ca37eabcf42344e30c32b4daf2f300ecf5217806
Jason Hensley <jasonhensley1989@gmail.com>
2023-04-30 11:22:17 -0500

    setting google auth

--- src/components/auth.js
+++ src/components/auth.js
@@ -6,8 +6,15 @@ const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  console.log(auth?.currentUser?.email);
+
   const signIn = async() => {
-    await createUserWithEmailAndPassword(auth, email, password);
+    try {
+        await createUserWithEmailAndPassword(auth, email, password);
+    } catch(err) {
+        console.error(err);
+    }
+    
   }
   
   const signInWithGoogle = () => {
@@ -22,7 +29,7 @@ const Auth = () => {
   return (
     <div className='userLogin'>
         <input placeholder="Email" type="text" className="userInput" onChange={(e) => setEmail(e.target.value)} />
-        <input placeholder="Password" type="text" className="userInput" onChange={(e) => setPassword(e.target.value)}/>
+        <input placeholder="Password" type="password" className="userInput" onChange={(e) => setPassword(e.target.value)}/>
         <button className="userBtn" onClick={signIn}>Sign In</button>
         <button className="userBtn" onClick={signInWithGoogle}>Sign In With Google</button>
         <button className="userBtn" onClick={signOut}>Sign Out</button>
