034c67ea82bb80dc91d06622fbdd9f67806a4e9f
Jason Hensley <jasonhensley1989@gmail.com>
2023-04-30 10:41:44 -0500

    react setup

---
+++ src/firebase-config/firebase.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB30tSZbXAVZ77tQQlVlLQYLnkqyq7ERII",
+  authDomain: "fir-react-176c4.firebaseapp.com",
+  projectId: "fir-react-176c4",
+  storageBucket: "fir-react-176c4.appspot.com",
+  messagingSenderId: "659034127905",
+  appId: "1:659034127905:web:6a15da5153bb92e9e4ed60"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
