db1bed51f2aa0cda5954954bbb7becdf640d3fdc
Jason Hensley <jasonhensley1989@gmail.com>
2023-04-30 11:15:24 -0500

    sign in auth on firebase

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { auth } from '../firebase-config/firebase';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 
 const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = async() => {
+    await createUserWithEmailAndPassword(auth, email, password);
+  }
+  
+  const signInWithGoogle = () => {
+    console.log("Click");
+  }
+
+  const signOut = () => { 
+    console.log("Click");
+  }
+
+
   return (
     <div className='userLogin'>
-        <input placeholder="Email" type="text" className="userInput" />
-        <input placeholder="Password" type="text" className="userInput" />
-        <button className="userBtn">Sign In</button>
-        <button className="userBtn">Sign In With Google</button>
-        <button className="userBtn">Sign Out</button>
+        <input placeholder="Email" type="text" className="userInput" onChange={(e) => setEmail(e.target.value)} />
+        <input placeholder="Password" type="text" className="userInput" onChange={(e) => setPassword(e.target.value)}/>
+        <button className="userBtn" onClick={signIn}>Sign In</button>
+        <button className="userBtn" onClick={signInWithGoogle}>Sign In With Google</button>
+        <button className="userBtn" onClick={signOut}>Sign Out</button>
     </div>
   )
 }
